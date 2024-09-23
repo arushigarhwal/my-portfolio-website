0feb2847b41ed76934930435a57d093090cb5c13
Arushi <arushigarhwal@gmail.com>
2024-09-23 22:31:26 +0530

    Updating the Navigation Bar

--- portfolio/src/components/navBar/navBar.js
+++ portfolio/src/components/navBar/navBar.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './navBar.css'
 import {Link} from 'react-scroll'
+
+import Resume from "../../assets/resume.pdf"
 const NavBar = () => {
+  const [navigateTo, setNavigateTo] = useState(null);
+  const [downloadResume, setDownloadResume] = useState(false);
+  
+  useEffect(() =>{
+    if (navigateTo){
+      window.location.href = navigateTo;
+    }
+  }, [navigateTo]);
+
+  useEffect(() => {
+    if (downloadResume) {
+      const link = document.createElement('a');
+      link.href = Resume; 
+      link.download = 'ArushiGarhwal.pdf';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      setDownloadResume(false); 
+    }
+  }, [downloadResume]);
   return (
     <div>
       <nav className='navbar'>
         <div className='nav'>
-          <Link className='items'>Home</Link>
-          <Link className='items'>About Me</Link>
-          <Link className='items'>Projects</Link>
+          <Link className='items' onClick={() => setNavigateTo('/')}>Home</Link>
+          <Link className='items' onClick={() => setNavigateTo('/aboutme')}>About Me</Link>
+          <Link className='items' onClick={() => setNavigateTo('/projects')}>Projects</Link>
         </div>
-        <button className='resume'>Resume</button>
-      </nav>
+        <button className='resume' onClick={() => setDownloadResume(true)}>Resume</button>
+        </nav>
     </div>
   )
 }
