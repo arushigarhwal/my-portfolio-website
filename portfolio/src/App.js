94e197dcd7088e62b66a53914bb5b272c64e5d6c
Arushi <arushigarhwal@gmail.com>
2024-09-23 23:36:52 +0530

    Adding the Intro Section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,20 +1,37 @@
-import NavBar from "./components/navBar/navBar";
-import Landing from "./components/landing/landing";
-import Skills from "./components/skills/skills";
-import Projects from "./components/projects/projects";
-import Footer from "./components/footer/footer";
-import Contact from "./components/contact/contact";
-function App() {
+import React from 'react';
+import NavBar from './components/navBar/navBar';
+import Landing from './components/landing/landing';
+import AboutMe from './components/aboutMe/aboutMe';
+import Projects from './components/projects/projects';
+import Contact from './components/contact/contact';
+import Footer from './components/footer/footer';
+import Intro from './components/intro/intro'
+
+const App = () => {
+  const renderPage = () => {
+    switch (window.location.pathname) {
+      case '/aboutme':
+        return <AboutMe />;
+      case '/projects':
+        return <Projects />;
+      default:
+        return (
+          <>
+            <Landing />
+            <Intro />
+            <Contact />
+            <Footer />
+          </>
+        );
+    }
+  };
+
   return (
     <div className="App">
-      <NavBar/ >
-      <Landing/ >
-      <Skills/ >
-      <Projects/ >
-      <Contact />
-      <Footer />
+      <NavBar />
+      {renderPage()}
     </div>
   );
-}
+};
 
 export default App;
---
+++ portfolio/src/components/intro/intro.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './intro.css'
+const Intro = () => {
+  return (
+    <section id='introduction'>
+        <h2 className="titleIntro">A Bit About Me!</h2>
+      <div className='desc'>Welcome to my digital playground! As a Computer Science and Engineering student, I’m passionate about crafting innovative solutions and diving into the exciting world of machine learning. From building clever algorithms to creating user-friendly applications, I’m on a mission to make tech fun and accessible! Join me on this wild journey of creativity and discovery. Let’s code some magic together! 🎉✨</div>
+    </section>
+  )
+}
+
+export default Intro
